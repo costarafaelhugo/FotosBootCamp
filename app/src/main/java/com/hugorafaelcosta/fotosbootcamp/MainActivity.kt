1bd270291359ea161f4049f26bf59c6d2e60af1c
Hugo Costa <costarafaelhugo@gmail.com>
2022-01-26 15:13:28 -0300

    Adicionei o controle de fluxo do botão openCamera

--- app/src/main/java/com/hugorafaelcosta/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/fotosbootcamp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hugorafaelcosta.fotosbootcamp
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -32,7 +33,35 @@ class MainActivity : AppCompatActivity() {
                 pickImageFromGAllery()
             }
         }
+
+        binding.openCameraButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED
+                ) {
+                    val permissions = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                    requestPermissions(permissions, PERMISSION_CODE_CAMERA_CAPTURE)
+                } else {
+                    openCamera()
+                }
+
+            } else {
+                openCamera()
+            }
+        }
     }
+
+    companion object {
+        private val PERMISSION_CODE_IMAGE_PICK = 1000
+        private val IMAGE_PICK_CODE = 1001
+        private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -45,9 +74,21 @@ class MainActivity : AppCompatActivity() {
             PERMISSION_CODE_IMAGE_PICK -> {
                 if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     pickImageFromGAllery()
+                } else {
+                    Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
                 }
-                Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
             }
+            PERMISSION_CODE_CAMERA_CAPTURE -> {
+                if (grantResults.size > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                            openCamera()
+                }
+                else{
+                    Toast.makeText(this, "Permissão Negada", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
         }
     }
 
@@ -59,14 +100,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             binding.imageView.setImageURI(data?.data)
         }
     }
 
-    companion object {
-        private val PERMISSION_CODE_IMAGE_PICK = 1000
-        private val IMAGE_PICK_CODE = 1001
 
-    }
 }
